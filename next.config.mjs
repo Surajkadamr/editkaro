dc1b4446ce6ab9659068ce659b3963d73d18686d
Surajkadamr <95098911+Surajkadamr@users.noreply.github.com>
2025-04-04 15:13:50 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 import path from 'path';
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'standalone',
-};
-export default nextConfig;
+    /** @type {import('next').NextConfig} */
+    const nextConfig = {
+        images: {
+          remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'images.unsplash.com',
+              port: '',
+              pathname: '/**', // Allows any path on this hostname
+            },
+            // You can add more objects here for other allowed domains
+            // e.g., { protocol: 'https', hostname: 'assets.example.com' }
+          ],
+        },
+      };
+  
+      export default nextConfig;
